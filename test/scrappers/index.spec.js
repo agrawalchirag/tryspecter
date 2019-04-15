5e7038f7e6a0c715125e1eb06fa3d06bd2d4072d
Chirag Agrawal <agrawal.chiragg420@gmail.com>
2019-04-15 14:06:26 +0530

    Added smoke tests

---
+++ test/scrappers/index.spec.js
@@ -0,0 +1,8 @@
+describe("scrapper dummy test cases", () => {
+  it("should return true", () => {
+    //Testing a boolean
+    expect(true).toBeTruthy();
+    //Another way to test a boolean
+    expect(true).toEqual(true);
+  });
+});
---
+++ test/worker/index.spec.js
@@ -0,0 +1,8 @@
+describe("worker dummy test cases", () => {
+  it("should return true", () => {
+    //Testing a boolean
+    expect(true).toBeTruthy();
+    //Another way to test a boolean
+    expect(true).toEqual(true);
+  });
+});
